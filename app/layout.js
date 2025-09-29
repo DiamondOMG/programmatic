9dd1c7d3828ad8f0f8448c46b7316ed822dc8b24
Mond Naja <omgdigital.developer.02@gmail.com>
2025-09-29 15:25:25 +0700

    icon

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Asset Management",
-  description: "Asset Management System",
+  title: "Programmatic",
+  description: "Programmatic System",
+  icons: {
+    icon: '/screen_icon_1.png',
+  },
 };
 
 export default function RootLayout({ children }) {
