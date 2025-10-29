4134446f92ac2b98621d85e06d849c25f2157afc
Mond Naja <omgdigital.developer.02@gmail.com>
2025-10-29 17:04:51 +0700

    feat(signage): enhance signage form conditions with AssetTypeId for better filtering

--- app/make_data/signage_form.js
+++ app/make_data/signage_form.js
@@ -1,10 +1,10 @@
 const signage_form = {
-    "TV Signage 43":"displayAspectRatio == \"1920x1080\"",
-    "TV Signage 55":"displayAspectRatio == \"1920x1080\"",
-    "Kiosk 43":"displayAspectRatio == \"1080x1920\"",
-    "Kiosk 55":"displayAspectRatio == \"1080x1920\"",
-    "Category Signage":"displayAspectRatio == \"1920x540\"",
-    "Pillar Signage":"displayAspectRatio == \"1080x1920\""
-}
+  "TV Signage 43": 'displayAspectRatio == "1920x1080" && AssetTypeId == "AS0119"',
+  "TV Signage 55": 'displayAspectRatio == "1920x1080" && AssetTypeId == "AS0120"',
+  "Kiosk 43": 'displayAspectRatio == "1080x1920" && AssetTypeId == "AS0119" ',
+  "Kiosk 55": 'displayAspectRatio == "1080x1920" && AssetTypeId == "AS0120" ',
+  "Category Signage": 'displayAspectRatio == "1920x540"&& AssetTypeId == "AS0066"',
+  "Pillar Signage":'displayAspectRatio == "1080x1920" && AssetTypeId == "AS0066"'
+};
 
 export default signage_form;
